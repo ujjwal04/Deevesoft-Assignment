11ef8476ed3e37173a2edb70a24e4845e5d838ff
ujjwal <ujjawalverma77@gmail.com>
2020-07-12 17:37:59 +0530

    Updates

---
+++ precache-manifest.64d0f6f765edc14848bc0d00d2ad4562.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39a8cc3188239f398deb1b9e9172882",
+    "url": "/Deevesoft-Assignment/index.html"
+  },
+  {
+    "revision": "b571ce3cb48a9ce7a7bc",
+    "url": "/Deevesoft-Assignment/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dbada11ef5c103ede62e",
+    "url": "/Deevesoft-Assignment/static/css/main.21866e0c.chunk.css"
+  },
+  {
+    "revision": "b571ce3cb48a9ce7a7bc",
+    "url": "/Deevesoft-Assignment/static/js/2.f095685b.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/Deevesoft-Assignment/static/js/2.f095685b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbada11ef5c103ede62e",
+    "url": "/Deevesoft-Assignment/static/js/main.63b8ccba.chunk.js"
+  },
+  {
+    "revision": "7189bdd0362482e2f0bf",
+    "url": "/Deevesoft-Assignment/static/js/runtime-main.1fc21a43.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Deevesoft-Assignment/static/media/logo.5d5d9eef.svg"
+  }
+]);
